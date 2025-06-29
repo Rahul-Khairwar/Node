b3972c6bcf8360c0dfaa7fd4c46bcf60c3cd7341
R1a2h3u4l5k <rahulkhairwar01@gmail.com>
2025-06-29 18:17:05 +0530

    data

---
+++ FullStack/fullstack2/client/src/App.jsx
@@ -0,0 +1,12 @@
+import axio from 'axios'
+import React from 'react'
+
+const app=()=>{
+    return(
+        <>
+    <h1>welcome to middleware</h1>
+        <button>Ok</button>
+        </>
+    )
+}
+export default app;
